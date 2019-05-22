4559097ae7627ff98de78e52ff34f7ee493bb6f7
uniqsoftco <50379296+uniqsoftco@users.noreply.github.com>
2019-05-22 22:51:09 +0430

    Day5-Part1-Completed

---
+++ Day5/1-WhatRemainsAfterReact/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_WhatRemainsAfterReact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("input.txt");
+            int reactCount = 0;
+
+            do
+            {
+                reactCount = 0;
+                for (int i = 0; i < input.Length - 1; ++i)
+                {
+                    if (input[i] - 32 == input[i + 1] || input[i] + 32 == input[i + 1])
+                    {
+                        input = input.Remove(i, 2);
+                        ++reactCount;
+                        --i;
+                    }
+                }
+            } while (reactCount > 0);
+
+            File.WriteAllText("out.txt", input);
+            Console.WriteLine($"Answer:  {input.Length}");
+            Console.ReadLine();
+        }
+    }
+}
